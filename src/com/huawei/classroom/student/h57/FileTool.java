73359a53d01c89cd7d9d5f9fab869bc3a52cc3ca
VMnK-Run <wang232519@tju.edu.cn>
2022-06-01 20:00:34 +0800

    h57 finished

--- src/com/huawei/classroom/student/h57/FileTool.java
+++ src/com/huawei/classroom/student/h57/FileTool.java
@@ -1,11 +1,33 @@
 package com.huawei.classroom.student.h57;
 
-public class FileTool {
+import java.io.*;
 
+public class FileTool {
+	
+	public long totalSum;
+	
 	/*
 	 * 统计一个目录下所有文件大小的加和
 	 */
 	public long recursiveCalcFileSize(String homeDir) {
-		return 0;
+		this.totalSum = 0;
+		File file = new File(homeDir);
+		ListFiles(file);
+		return this.totalSum;
+	}
+	
+	public void ListFiles(File dir) {
+		if(!dir.exists() || !dir.isDirectory()) {
+			return;
+		}
+		String[] files = dir.list();
+		for(int i = 0; i < files.length; i++) {
+			File file = new File(dir, files[i]);
+			if(file.isFile()) {
+				this.totalSum += file.length();
+			} else {
+				ListFiles(file);
+			}
+		}
 	}
 }
--- src/com/huawei/classroom/student/h57/Test.java
+++ src/com/huawei/classroom/student/h57/Test.java
@@ -7,9 +7,11 @@ public class Test {
 		//递归统计一个目录下所有文件的大小的加和
 		FileTool  tool=new FileTool ();
 		//此题目要求精确匹配答案
-		long length=tool.recursiveCalcFileSize("xxxx/test_dir/");
+		long length=tool.recursiveCalcFileSize("D:\\北小洋\\21222\\程序设计初级实践\\OOP_Junior_Practice\\src\\com\\huawei\\classroom\\student\\h57\\test_dir\\");
 		if(length==132) {
 			System.out.println("pass ");
+		} else {
+			System.out.println("傻逼");
 		}
 		System.out.println(length);
 	}
